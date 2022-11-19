2b3e8205904d7a38ad3fb2be7d222c62c64e36aa
Halocline <mdglissmann@gmail.com>
2022-11-19 13:11:48 -0700

    bunch of stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,26 @@
 import './globals.css'
+import { Footer, Grommet, Header, Main } from '../components/Grommet'
+import { hpe } from '../themes/hpe'
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      {/*
+    return (
+        <html lang="en">
+            {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+            <head />
+            <body>
+                <Grommet theme={hpe} themeMode="dark" full>
+                    <Header background="background-contrast">My header</Header>
+                    <Main>{children}</Main>
+                    <Footer background="background-contrast">My footer</Footer>
+                </Grommet>
+            </body>
+        </html>
+    )
 }
