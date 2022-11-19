f0eaaac955bb8485f9b1ca1ee532cfcf248e5154
Halocline <mdglissmann@gmail.com>
2022-11-19 15:12:43 -0700

    move ...rest to Card

--- components/NavCard.tsx
+++ components/NavCard.tsx
@@ -6,24 +6,27 @@ import { Card } from './Grommet';
 
 export const NavCard = ({
   children,
+  key,
   index,
   href,
   ...rest
 }: {
   children: React.ReactNode;
+  key: string | number;
   index: number;
   href: string;
 }) => {
   const [hover, setHover] = useState(false);
 
   return (
-    <Link href={href} {...rest}>
+    <Link href={href} key={key}>
       <Card
         animation={{ type: 'fadeIn', delay: index * 50, duration: 500 }}
         elevation={hover ? 'medium' : 'small'}
         onMouseEnter={() => setHover(true)}
         onMouseLeave={() => setHover(false)}
         onClick={() => {}}
+        {...rest}
       >
         {children}
       </Card>
